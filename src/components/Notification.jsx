52b53b2f0a32a66c37cc785ff412517955ae442e
HarryShengCS <harryshengcoding@gmail.com>
2024-12-08 18:29:31 +1300

    feat: notification component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,11 @@
+import "../style/Notification.css";
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="error">{message}</div>;
+};
+
+export default Notification;
