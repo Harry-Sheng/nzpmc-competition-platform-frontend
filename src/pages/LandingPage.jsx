0b7c7e4b19d1a18764d64e5da35302e78af713b2
HarryShengCS <harryshengcoding@gmail.com>
2024-12-09 22:42:51 +1300

    chore: removed unnecessary imports

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import LandingPageLogged from "./LandingPageLogged";
 import LandingPageNotLogged from "./LandingPageNotLogged";
