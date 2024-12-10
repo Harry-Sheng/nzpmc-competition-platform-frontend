72af190736ef0f360ffba42cfc5c86cd75773e3a
HarryShengCS <harryshengcoding@gmail.com>
2024-12-10 20:17:07 +1300

    feat: implemented create event form

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,78 +3,107 @@ import Exam from "../assets/exam.png";
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import eventService from "../services/Events";
-import userService from "../services/User";
+import Notification from "../components/Notification";
+import CreateEventForm from "../components/CreateEventForm";
 
 const AdminDashboard = ({ events, users }) => {
-  const { user, setUser } = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleCreateEvent = async (event) => {
+    event.preventDefault();
+    try {
+      const createdEvent = await eventService.create({
+        name,
+        description,
+        date,
+      });
+      console.log("Event created successfully:", createdEvent);
+      setName("");
+      setDescription("");
+      setDate("");
+    } catch (error) {
+      setErrorMessage("Event already exists");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
 
   return (
-    <Row className="g-4">
-      <Col md={9}>
-        {/* Events Section */}
-        <h2 className="mb-4"> Events</h2>
-        {events.map((event, index) => (
-          <Card key={index} className="mb-3 shadow-sm rounded">
-            <Card.Body className="d-flex align-items-center">
-              <Col xs={3}>
-                <img
-                  src={Exam}
-                  alt="Event"
-                  className="img-fluid rounded"
-                  style={{ maxHeight: "100px", objectFit: "cover" }}
-                />
-              </Col>
-              <Col xs={7} className="ps-3">
-                <h5 className="mb-1">{event.name}</h5>
-                <p className="text-muted mb-1">{event.description}</p>
-                <p className="text-muted mb-0">{event.date}</p>
-              </Col>
-            </Card.Body>
-          </Card>
-        ))}
+    <>
+      <Notification message={errorMessage} variant="danger" />
+      <Row className="g-4">
+        <Col md={8}>
+          {/* Events Section */}
+          <h2 className="mb-4"> Events</h2>
+          {events.map((event, index) => (
+            <Card key={index} className="mb-3 shadow-sm rounded">
+              <Card.Body className="d-flex align-items-center">
+                <Col xs={3}>
+                  <img
+                    src={Exam}
+                    alt="Event"
+                    className="img-fluid rounded"
+                    style={{ maxHeight: "100px", objectFit: "cover" }}
+                  />
+                </Col>
+                <Col xs={7} className="ps-3">
+                  <h5 className="mb-1">{event.name}</h5>
+                  <p className="text-muted mb-1">{event.description}</p>
+                  <p className="text-muted mb-0">{event.date}</p>
+                </Col>
+              </Card.Body>
+            </Card>
+          ))}
 
-        {/* Accounts Section */}
-        <h2 className="mb-4"> Accounts</h2>
-        {users.map((user, index) => (
-          <Card key={index} className="mb-3 shadow-sm rounded">
-            <Card.Body className="d-flex align-items-center">
-              <Col xs={3}>
-                <img
-                  src={Exam}
-                  alt="Event"
-                  className="img-fluid rounded"
-                  style={{ maxHeight: "100px", objectFit: "cover" }}
-                />
-              </Col>
-              <Col xs={7} className="ps-3">
-                <h5 className="mb-1">{user.name}</h5>
-                <p className="text-muted mb-1">{user.email}</p>
-                {user.events && user.events.length > 0 ? (
-                  user.events.map((event, eventIndex) => (
-                    <p key={eventIndex} className="text-muted mb-0">
-                      {event.name}
-                    </p>
-                  ))
-                ) : (
-                  <p className="text-muted mb-0">No Events Available</p>
-                )}
-              </Col>
-            </Card.Body>
-          </Card>
-        ))}
-      </Col>
+          {/* Accounts Section */}
+          <h2 className="mb-4"> Accounts</h2>
+          {users.map((user, index) => (
+            <Card key={index} className="mb-3 shadow-sm rounded">
+              <Card.Body className="d-flex align-items-center">
+                <Col xs={3}>
+                  <img
+                    src={Exam}
+                    alt="Event"
+                    className="img-fluid rounded"
+                    style={{ maxHeight: "100px", objectFit: "cover" }}
+                  />
+                </Col>
+                <Col xs={7} className="ps-3">
+                  <h5 className="mb-1">{user.name}</h5>
+                  <p className="text-muted mb-1">{user.email}</p>
+                  {user.events && user.events.length > 0 ? (
+                    user.events.map((event, eventIndex) => (
+                      <p key={eventIndex} className="text-muted mb-0">
+                        {event.name}
+                      </p>
+                    ))
+                  ) : (
+                    <p className="text-muted mb-0">No Events Available</p>
+                  )}
+                </Col>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
 
-      {/* User Details Section */}
-      <Col md={3}>
-        <Card>
-          <Card.Body>
-            <Button variant="secondary" onClick={() => setShowModal(true)}>
-              Edit Name
-            </Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+        {/* Create event Section */}
+        <Col md={4}>
+          <CreateEventForm
+            name={name}
+            description={description}
+            date={date}
+            setName={setName}
+            setDescription={setDescription}
+            setDate={setDate}
+            handleCreateEvent={handleCreateEvent}
+          />
+        </Col>
+      </Row>
+    </>
   );
 };
 
---
+++ src/components/CreateEventForm.jsx
@@ -0,0 +1,53 @@
+import { Form, Button } from "react-bootstrap";
+
+const CreateEventForm = ({
+  name,
+  description,
+  date,
+  setName,
+  setDescription,
+  setDate,
+  handleCreateEvent,
+}) => {
+  return (
+    <Form
+      onSubmit={handleCreateEvent}
+      className="p-4 border rounded shadow-sm bg-light"
+      style={{ margin: "auto" }}
+    >
+      <h2 className="text-center mb-4">Create Event</h2>
+      <Form.Group className="mb-3" controlId="eventName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter event name"
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="eventDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter event description"
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="eventDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter event date"
+          value={date}
+          onChange={({ target }) => setDate(target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" className="w-100">
+        Create
+      </Button>
+    </Form>
+  );
+};
+
+export default CreateEventForm;
