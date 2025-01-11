f41385469117566baf01a8dfe1ece908fbf80826
HarryShengCS <harryshengcoding@gmail.com>
2025-01-11 16:32:59 +1300

    feat (competition side bar): implement scrollable to side bar.

--- src/components/CompetitionSideBar.jsx
+++ src/components/CompetitionSideBar.jsx
@@ -15,24 +15,26 @@ export function CompetitionSideBar({
       <Card.Body>
         <h5>Time: {formatTime(timeElapsed)}</h5>
         <ProgressBar now={progress} variant="success" className="mb-3" />
-        <div className="d-grid gap-2">
-          {questions.map((question, index) => (
-            <Button
-              key={index}
-              variant={
-                currentQuestion === index ? "primary" : "outline-primary"
-              }
-              className="text-start position-relative"
-              onClick={() => handleQuestionSelect(index)}
-            >
-              Question {index + 1}
-              {selectedAnswers.get(questions[index].title) !== -1 && (
-                <span className="position-absolute top-50 end-0 translate-middle-y me-2 text-success">
-                  ✓
-                </span>
-              )}
-            </Button>
-          ))}
+        <div className="d-grid ">
+          <div className="scrollable-xsmall">
+            {questions.map((question, index) => (
+              <Button
+                key={index}
+                variant={
+                  currentQuestion === index ? "primary" : "outline-primary"
+                }
+                className="text-start position-relative"
+                onClick={() => handleQuestionSelect(index)}
+              >
+                Question {index + 1}
+                {selectedAnswers.get(questions[index].title) !== -1 && (
+                  <span className="position-absolute top-50 end-0 translate-middle-y me-2 text-success">
+                    ✓
+                  </span>
+                )}
+              </Button>
+            ))}
+          </div>
           <Button variant="success" className="mt-3" onClick={handleSubmit}>
             Submit
           </Button>
